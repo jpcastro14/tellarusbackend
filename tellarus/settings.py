1c15ff0f986a2cf6e86a74b111092603ed20dcb5
jpcastro14 <castrovoip@gmail.com>
2025-01-10 18:33:45 -0300

    Performing the first POST api request, fixing CORS issues

--- tellarus/settings.py
+++ tellarus/settings.py
@@ -30,7 +30,11 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,10 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chamados',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
